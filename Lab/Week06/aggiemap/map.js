2b10cb417a76a850d814cafc0e07d08efde67589
Martin Garret <martin.garret@tamu.edu>
2026-04-30 16:40:46 -0500

    added project folder aggiemap

---
+++ Lab/Week06/aggiemap/map.js
@@ -0,0 +1,119 @@
+
+      // TAMU coordinates: 30.6188, -96.3365
+      var map = L.map('mapid').setView([30.6121, -96.3455], 15);
+   
+      
+      // Using OpenStreetMap tiles instead of Mapbox (no token needed)
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 19
+}).addTo(map);
+
+fetch("https://services1.arcgis.com/qr14biwnHA6Vis6l/arcgis/rest/services/tamubuildings_garret/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson")
+  .then(response => response.json())
+  .then(data => {
+
+    console.log(data.features[0].properties);
+
+    buildingsLayer = L.geoJSON(data, {
+  style: style,
+  onEachFeature: onEachFeature
+}).addTo(map);
+
+  })
+  .catch(error => console.error("Error loading data:", error));
+
+      // Create info control
+      var info = L.control();
+      
+      info.onAdd = function(map) {
+        this._div = L.DomUtil.create('div', 'info');
+        this.update();
+        return this._div;
+      };
+      
+      info.update = function(props) {
+        this._div.innerHTML = '<h4>TAMU Building Info</h4>' + 
+          (props ? 
+            '<b>' + props.BldgName + '</b><br />Abbreviation: ' + props.BldgAbbr :
+            'Hover over a building');
+      };
+      
+      info.addTo(map);
+      
+      // Style for buildings
+      function style(feature) {
+        return {
+          weight: 2,
+          opacity: 1,
+          color: '#500000', // Maroon color
+          dashArray: '3',
+          fillOpacity: 0.7,
+          fillColor: '#500000' // Maroon fill
+        };
+      }
+      
+      // Highlight function
+      function highlightFeature(e) {
+        var layer = e.target;
+        
+        layer.setStyle({
+          weight: 3,
+          color: '#FFD700', //  yellow border
+          dashArray: '',
+          fillOpacity: 0.9
+        });
+        
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+          layer.bringToFront();
+        }
+        
+        info.update(layer.feature.properties);
+      }
+      
+      // Reset highlight
+      function resetHighlight(e) {
+        buildingsLayer.resetStyle(e.target);
+        info.update();
+      }
+      
+      // Zoom to feature
+      function zoomToFeature(e) {
+        map.fitBounds(e.target.getBounds());
+      }
+      
+      // Set listeners for each feature
+      function onEachFeature(feature, layer) {
+        layer.on({
+          mouseover: highlightFeature,
+          mouseout: resetHighlight,
+          click: zoomToFeature
+        });
+      }
+      
+      
+    // Invalidate size after a short delay to ensure proper rendering
+    window.addEventListener("load", function () {
+    setTimeout(() => {
+        map.invalidateSize();
+    }, 300);
+});
+function searchBuilding() {
+  var input = document.getElementById("searchInput").value.toLowerCase();
+
+  var found = false;
+
+  buildingsLayer.eachLayer(function (layer) {
+    var name = layer.feature.properties.BldgName;
+
+    if (name && name.toLowerCase().includes(input)) {
+      map.fitBounds(layer.getBounds());
+      layer.openPopup();
+      found = true;
+    }
+  });
+
+  if (!found) {
+    alert("Building not found");
+  }
+}
