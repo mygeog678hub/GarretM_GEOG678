6809fc51e97ebd5c4f0bbe9a2fdcc1bec64b6eff
Martin Garret <martin.garret@tamu.edu>
2026-05-01 01:39:24 -0500

    .

--- Projects/Interactive_Crime_Map/map.js
+++ Projects/Interactive_Crime_Map/map.js
@@ -9,86 +9,57 @@ var heatLayer;
 var crimePoints = [];
 
 // ✅ REAL DATASETS
-var cityServices = {
-  houston: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Houston_Crime_Data/FeatureServer/0",
-  austin: "https://services.arcgis.com/0L95CJ0VTaxqcmED/arcgis/rest/services/Austin_Crime_Data/FeatureServer/0"
-};
+var dataUrl = "https://raw.githubusercontent.com/glynnbird/usstatesgeojson/master/california.geojson";
 
 function loadCityData() {
-
-  var city = document.getElementById("citySelect").value;
-  var url = cityServices[city];
 
   clusterLayer.clearLayers();
   crimePoints = [];
 
-  fetch(url + "/query?where=1=1&outFields=*&outSR=4326&f=geojson")
-  .then(res => res.json())   // ✅ THIS WAS MISSING
-  .then(data => {
+  fetch(dataUrl)
+    .then(res => res.json())
+    .then(data => {
 
-    console.log("DATA:", data);
+      console.log("DATA:", data);
 
-    if (!data.features || data.features.length === 0) {
-      console.error("No features returned from service");
-      return;
-    }
+      data.features.forEach(f => {
 
-    data.features.forEach(f => {
+        var coords = f.geometry.coordinates[0][0];
+        var latlng = [coords[1], coords[0]];
 
-      if (!f.geometry) return;
+        var marker = L.circleMarker(latlng, {
+          radius: 6,
+          fillColor: "blue",
+          color: "#000",
+          weight: 1,
+          fillOpacity: 0.8
+        });
 
-      var latlng;
+        marker.bindPopup("Test Feature");
 
-      // GeoJSON
-      if (f.geometry.coordinates) {
-        latlng = [f.geometry.coordinates[1], f.geometry.coordinates[0]];
-      }
+        clusterLayer.addLayer(marker);
+        crimePoints.push(latlng);
 
-      // ArcGIS
-      else if (f.geometry.x && f.geometry.y) {
-        latlng = [f.geometry.y, f.geometry.x];
-      }
-
-      else {
-        return;
-      }
-
-      var type = getType(f);
-
-      var marker = L.circleMarker(latlng, {
-        radius: 5,
-        fillColor: getColor(type),
-        color: "#000",
-        weight: 1,
-        fillOpacity: 0.8
       });
 
-      marker.bindPopup(`<b>${type}</b><br>${getDescription(f)}`);
+      map.addLayer(clusterLayer);
+      buildHeat();
 
-      clusterLayer.addLayer(marker);
-      crimePoints.push(latlng);
+    })
+    .catch(err => console.error("FETCH ERROR:", err));
 
-    });
-
-    map.addLayer(clusterLayer);
-    map.setView(getCityCenter(city), 12);
-    buildHeat();
-
-  })
-  .catch(err => console.error("FETCH ERROR:", err));
 }
-
 function getType(f) {
-  return f.properties.offense ||
-         f.properties.offense_type ||
-         f.properties.incident_type ||
-         f.properties.ucr_description ||
+  return f.attributes?.offense ||
+         f.attributes?.offense_type ||
+         f.attributes?.incident_type ||
+         f.attributes?.ucr_description ||
          "Other";
 }
 
 function getDescription(f) {
-  return f.properties.offense_desc ||
-         f.properties.description ||
+  return f.attributes?.offense_desc ||
+         f.attributes?.description ||
          "No description";
 }
 
